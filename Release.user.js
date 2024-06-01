035c239117767829ca2db9e6a5826f4955148249
trigger-off <71810229+trigger-off@users.noreply.github.com>
2024-06-01 22:24:09 +0500

    Update Release.user.js

--- Release.user.js
+++ Release.user.js
@@ -5,12 +5,8 @@
 // @description  hack valley 
 // @author       Triggeroff
 // @match        *://*/*
-// @grant        GM_addStyle
-// @require      https://code.jquery.com/jquery-2.1.4.min.js
-// @require      https://code.jquery.com/ui/1.11.4/jquery-ui.min.js
-// @require      https://raw.githubusercontent.com/trigger-off/valley/main/Hack.js
-// @require      https://raw.githubusercontent.com/trigger-off/tampermenu/master/menu.js
-// @run-at       document-end
+// @grant        none
+// @require      https://github.com/trigger-off/valley/raw/main/Hack.js
 // ==/UserScript==
 
 (function() {
@@ -22,28 +18,9 @@
         },3500)
         
     } else {
-        
         waitForElm('#bottomMenu > div > div.button.settings > div').then((elm) => {
             console.log('Script is ready');
-            var external$uiCss = "ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/dark-hive/jquery-ui.css";
-            $("head").append(
-                '<link href="//'
-                +  external$uiCss
-                + '" rel="stylesheet" type="text/css">'
-            );
-
-            var menuHandler = createMenu({
-                textName: "Inject",
-                items: [{
-                    menuText: "DO IT!",
-                    iconClass: "ui-icon-heart",
-                    onClick: function(){
-                    alert("hi");
-                    executeScript();
-                    }
-                }]
-            });
-            
+            executeScript();
         });
 
     }
