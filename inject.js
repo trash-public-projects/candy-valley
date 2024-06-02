d68eb7caa7c8cd83d1d3ecbba71b86da5554211e
trigger-off <71810229+trigger-off@users.noreply.github.com>
2024-06-03 01:52:33 +0500

    Update inject.js

--- inject.js
+++ inject.js
@@ -2,6 +2,6 @@ setTimeout(() => {
     let script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = "https://raw.githubusercontent.com/trigger-off/valley/main/download-us.js";
-
+    document.head.appendChild(script);
 },5000)
 
