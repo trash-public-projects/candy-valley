c049515d5c5af3311b08c10d00647598bed5dbe2
trigger-off <71810229+trigger-off@users.noreply.github.com>
2024-06-03 01:41:18 +0500

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,22 +1,26 @@
-function waitForElm(selector) {
-    return new Promise(resolve => {
-        if (document.querySelector(selector)) {
-            return resolve(document.querySelector(selector));
-        }
-        const observer = new MutationObserver(mutations => {
+setTimeout(() => {
+    function waitForElm(selector) {
+        return new Promise(resolve => {
             if (document.querySelector(selector)) {
-                observer.disconnect();
-                resolve(document.querySelector(selector));
+                return resolve(document.querySelector(selector));
             }
+            const observer = new MutationObserver(mutations => {
+                if (document.querySelector(selector)) {
+                    observer.disconnect();
+                    resolve(document.querySelector(selector));
+                }
+            });
+            observer.observe(document.body, {
+                childList: true,
+                subtree: true
+            });
         });
-        observer.observe(document.body, {
-            childList: true,
-            subtree: true
-        });
-    });
-}
-waitForElm("iframe").then((elm) => {
-            setTimeout(() => {
-                window.location.replace("https://raw.githubusercontent.com/trigger-off/valley/main/Release.user.js");
-            },10000)
-})
+    }
+    waitForElm("iframe").then((elm) => {
+        setTimeout(() => {
+            window.location.replace("https://raw.githubusercontent.com/trigger-off/valley/main/Release.user.js");
+        },10000)
+    })
+    
+},5000)
+
