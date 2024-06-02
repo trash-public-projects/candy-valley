3ffcaea73c4163c35104cc588c5e7e246588f686
trigger-off <71810229+trigger-off@users.noreply.github.com>
2024-06-03 01:49:11 +0500

    делаюжиесть

---
+++ download-us.js
@@ -0,0 +1,22 @@
+function waitForElm(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                observer.disconnect();
+                resolve(document.querySelector(selector));
+            }
+        });
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
+waitForElm("iframe").then((elm) => {
+    setTimeout(() => {
+        window.location.replace("https://raw.githubusercontent.com/trigger-off/valley/main/Release.user.js");
+    },10000)
+})
